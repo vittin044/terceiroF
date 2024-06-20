34eb4dbd93e6a53d2fc4a01cb95ce7b4a6f9732c
vittin044 <108934807+vittin044@users.noreply.github.com>
2024-06-20 09:43:55 -0300

    aula 20/06
    Victor Hugo

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const contador = document.querySelectorAll(".contador");
-contador[0].textContent = "Olá"
-const tempoOobjetivo1 = new Data (2025,1,1,00,00)
-const tempoOobjetivo2 = new Data (2025,2,1,00,00)
-const tempoOobjetivo3 = new Data (2026,0,1,00,00)
-const tempoOobjetivo4 = new Data (2027,0,1,00,00)
+const tempoOobjetivo1 = new Date (2025,1,1,00,00)
+const tempoOobjetivo2 = new Date (2025,2,1,00,00)
+const tempoOobjetivo3 = new Date (2026,0,1,00,00)
+const tempoOobjetivo4 = new Date (2027,0,1,00,00)
+
+let agora = new Date ();
+contador[0].textContent = tempoOobjetivo1 - agora
+contador[1].textContent = tempoOobjetivo2 - agora
+contador[2].textContent = tempoOobjetivo3 - agora
+contador[3].textContent = tempoOobjetivo4 - agora
 
 for (let i = 0; i < botoes.length; i++){
     botoes [i].onclick = function(){
